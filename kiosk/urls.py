3e0b009ff34cf565cf1c1e8deeb80a194348fbef
Jon M. Dugan <jdugan@x1024.net>
2013-10-07 23:16:32 -0500

    rework UI to use primarily Twitter Bootstrap; forms to add popups and pages

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('kiosk.views',
         url(r'^_loc/(?P<page>[a-zA-Z0-9-]+)/?$', 'loc_data', name="kiosk-loc-data"),
         url(r'^_popup/(?P<page>[a-zA-Z0-9-]+)/?$', 'popup_data', name="kiosk-popup-data"),
         url(r'^_links/?$', 'link_data', name="kiosk-link-data"),
+        url(r'^_update_page_background/(?P<page>[a-zA-z0-9-]+)/?$', 'update_page_background', name='update-page-background'),
+        url(r'^_kiosk_item/(?P<item>[a-zA-z0-9-]+)?/?$', 'kiosk_item', name='kiosk-item'),
 )
--- kiosk/views.py
+++ kiosk/views.py
@@ -4,13 +4,14 @@ from django.http import Http404, HttpResponseRedirect, HttpResponse, HttpRespons
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 
 try:
     import json
 except ImportError:
     import simplejson as json
 
-from kiosk.models import KioskItem, KioskPageLinkLocation
+from kiosk.models import KioskItem, KioskPageLinkLocation, KioskItemForm
 
 def index(request, page=None):
     if not page:
@@ -75,4 +76,50 @@ def link_data(request):
         else:
             l.append(('#' + item.name, item.name))
 
+    l.sort(key=lambda x: x[1])
+
     return HttpResponse(json.dumps(l))
+
+def update_page_background(request, page):
+    page = get_object_or_404(KioskItem, name=page, type='page')
+
+
+    r = {}
+    if request.FILES and request.FILES.get('file_upload'):
+        f = request.FILES.get('file_upload')
+        page.page_image = f
+        page.save()
+        r['status'] = "OK"
+    else:
+        r['status'] = "Error: no file attached"
+
+    return HttpResponse(json.dumps(r))
+
+def kiosk_item(request, item=None):
+    r = {}
+    if request.method == "GET":
+        obj = get_object_or_404(KioskItem, name=item)
+        return HttpResponse(serializers.serialize("json", [obj]).strip("[]"))
+    elif request.method == "POST":
+        print json.dumps(request.POST, indent=4)
+        for f in request.FILES:
+            print f
+
+        f = KioskItemForm(request.POST, request.FILES)
+        print "VALID?", f.is_valid()
+        if not f.is_valid():
+            print "ERR", f.errors
+            errdict = { 'status' : "Error",
+                       'errors' : [(k, v) for k, v in f.errors.items()] }
+            print errdict
+            return HttpResponse(json.dumps(errdict))
+
+        obj = f.save()
+        if obj.type == 'page':
+            link = "/" + obj.name
+        else:
+            link = "#" + obj.name
+        r['status'] = "OK"
+        r['link'] = link
+
+    return HttpResponse(json.dumps(dict(status="OK", link=link)))
