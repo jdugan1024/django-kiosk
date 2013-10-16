d00fe7cdec14a562ff0744ea45bdda4c1a985fa7
Jon M. Dugan <jdugan@x1024.net>
2013-10-16 16:34:30 -0500

    remove another unused/old view and url

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -4,8 +4,7 @@ urlpatterns = patterns('kiosk.views',
         # index
         url(r'^(?P<page>[a-zA-Z0-9-]+)?/?$', 'index', name="kiosk-index"),
         url(r'^_loc/(?P<page_name>[a-zA-Z0-9-]+)(/(?P<pk>[0-9]+))?/?$', 'loc_data', name="kiosk-loc-data"),
-        url(r'^_links/?$', 'link_data', name="kiosk-link-data"),
         url(r'^_kiosk_item/((?P<item_type>[a-zA-Z0-9-]+)/(?P<item_name>[a-zA-z0-9-]+))?/?$', 'kiosk_item', name='kiosk-item'),
-        url(r'^_update_kiosk_item_image/(?P<item_type>[a-zA-z0-9-]+)/(?P<item_name>[a-zA-z0-9]+)/?$', 
-        	'update_kiosk_item_image', name='update-kiosk-item-image'),
+        url(r'^_kiosk_item_image/(?P<item_type>[a-zA-z0-9-]+)/(?P<item_name>[a-zA-z0-9]+)/?$', 
+        	'kiosk_item_image', name='kiosk-item-image'),
 )
