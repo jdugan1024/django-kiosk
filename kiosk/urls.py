b34d82977f0c4eab6a2fafbc564a40b0d703ec4e
Jon M. Dugan <jdugan@x1024.net>
2013-11-01 16:53:41 -0500

    bugfix: allow images for items with - in them

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('kiosk.views',
         url(r'^(?P<page>[a-zA-Z0-9-]+)?/?$', 'index', name="kiosk-index"),
         url(r'^_loc/(?P<page_name>[a-zA-Z0-9-]+)(/(?P<pk>[0-9]+))?/?$', 'loc_data', name="kiosk-loc-data"),
         url(r'^_kiosk_item/((?P<item_type>[a-zA-Z0-9-]+)/(?P<item_name>[a-zA-z0-9-]+))?/?$', 'kiosk_item', name='kiosk-item'),
-        url(r'^_kiosk_item_image/(?P<item_type>[a-zA-z0-9-]+)/(?P<item_name>[a-zA-z0-9]+)/?$', 
+        url(r'^_kiosk_item_image/(?P<item_type>[a-zA-z0-9-]+)/(?P<item_name>[a-zA-z0-9-]+)/?$', 
         	'kiosk_item_image', name='kiosk-item-image'),
 )
